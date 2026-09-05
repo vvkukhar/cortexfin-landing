10e8d9cd27ad5b30de785a6cccc401de4d911362
Vlad <vladkuhar22082007@gmail.com>
2026-09-05 16:39:23 +0300

    Fix next-intl createNavigation API update

--- cortexfin-web/src/i18n/routing.ts
+++ cortexfin-web/src/i18n/routing.ts
@@ -1,9 +1,9 @@
 import {defineRouting} from 'next-intl/routing';
-import {createSharedPathnamesNavigation} from 'next-intl/navigation';
+import {createNavigation} from 'next-intl/navigation';
  
 export const routing = defineRouting({
   locales: ['en', 'uk'],
   defaultLocale: 'uk'
 });
  
-export const {Link, redirect, usePathname, useRouter} = createSharedPathnamesNavigation(routing);
+export const {Link, redirect, usePathname, useRouter} = createNavigation(routing);
